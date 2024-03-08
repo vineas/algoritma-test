1c5b7b54df04678ac4c25dc26bcb278c27f3bc7a
vineas <alvienasyandika@gmail.com>
2024-03-08 16:46:05 +0700

    nomer 4

--- algoritma.js
+++ algoritma.js
@@ -49,4 +49,20 @@ console.log(result)
 
 
 
-// nomer 4
+// // nomer 4
+function diagonalDifference(matrix) {
+    const n = matrix.length
+    let diagonal1 = 0
+    let diagonal2 = 0
+
+    for (let i = 0; i < n; i++) {
+        diagonal1 += matrix[i][i]
+        diagonal2 += matrix[i][n - i - 1]
+    }
+
+    return Math.abs(diagonal1 - diagonal2)
+}
+
+const matrix = [[1, 2, 0], [4, 5, 6], [7, 8, 9]]
+const result = diagonalDifference(matrix)
+console.log(result)
